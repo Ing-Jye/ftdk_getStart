b7e19827419c40370cdb0184d2c7b0813d77b5d5
Ing-Jye Lai <ftdk27@gmail.com>
2020-10-20 11:43:46 +0800

    test

--- hello.py
+++
@@ -1,5 +0,0 @@
-print("Hello py")
-
-print("chan in work dic")
-
-print("I also make exchange on hello.py")
--- sample2.py
+++ sample2.py
@@ -1,5 +1,2 @@
 print("This is sample 2")
 
-print("I make a change on sample 2")
-
-print("working dic and repo")
